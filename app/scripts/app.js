fbc57c0b3e893d11843dc2b8ca84f30a70c4678e
Li Yiting-YITING <yiting@motorola.com>
2015-07-19 22:55:39 -0700

    schema done

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'akoenig.deckgrid'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,39 @@
  * Controller of the yiting007githubioApp
  */
 angular.module('yiting007githubioApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+    var self = this;
+    // Put a reference to the controller into $scope so that all the data and methods needed in the
+    // scope can go into the controller.
+    $scope.mainCtrl = self;
+
+    self.photos = [{
+      id: 'photo-1',
+      name: 'Date Range Picker',
+      src: '/images/dateRangePicker.png'
+    }, {
+      id: 'photo-2',
+      name: 'Game of Life',
+      src: '/images/gameOfLife.png'
+    }, {
+      id: 'photo-3',
+      name: 'Java 3D Engine',
+      src: '/images/3dEngine.png'
+    }, {
+      id: 'photo-4',
+      name: 'Five Chess Game Simulator & AI',
+      src: '/images/5ChessGame.png'
+    }, {
+      id: 'photo-5',
+      name: 'Sudoku Solver',
+      src: '/images/sudokuSolver.png'
+    }, {
+      id: 'photo-6',
+      name: 'Golang & Arduino',
+      src: '/images/arduinoGo.gif'
+    }, {
+      id: 'photo-7',
+      name: 'Digit Recognition',
+      src: '/images/digitRec.png'
+    }];
   });
