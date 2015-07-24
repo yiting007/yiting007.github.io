a27376fb1ba5cdf4013649e7e3b4a7587a544aad
Li Yiting-YITING <yiting@motorola.com>
2015-07-23 21:17:35 -0700

    07/23/2015
    arduino page done

--- app/scripts/controllers/arduino.js
+++ app/scripts/controllers/arduino.js
@@ -8,5 +8,45 @@
  * Controller of the yiting007githubioApp
  */
 angular.module('yiting007githubioApp')
-  .controller('arduinoCtrl', function () {
-});
+  .controller('arduinoCtrl', function ($scope) {
+
+    $scope.editorOptionsGo = {
+      mode: 'go',
+      lineNumbers: true,
+      readOnly: 'true',
+    };
+
+    $scope.goCode = '' +
+    'package main\n\n' +
+
+    'import (\n' +
+    '    "fmt"\n' +
+    '    "time"\n' +
+    '    "github.com/hybridgroup/gobot"\n' +
+    '    "github.com/hybridgroup/gobot/platforms/firmata"\n' +
+    '    "github.com/hybridgroup/gobot/platforms/gpio"\n' +
+    ')\n\n' +
+
+    'func main() {\n' +
+    '    gbot := gobot.NewGobot()\n\n' +
+
+    '    firmataAdaptor := firmata.NewFirmataAdaptor("firmata", "/dev/tty.usbmodem1411")\n' +
+    '    servo := gpio.NewServoDriver(firmataAdaptor, "servo", "7")\n\n' +
+
+    '    work := func() {\n' +
+    '        gobot.Every(500*time.Millisecond, func() {\n' +
+    '            i := uint8(gobot.Rand(100) + 50)\n' +
+    '            fmt.Println("Turning", i)\n' +
+    '            servo.Move(i)\n' +
+    '        })\n' +
+    '    }\n' +
+    '    robot := gobot.NewRobot("servoBot",\n' +
+    '        []gobot.Connection{firmataAdaptor},\n' +
+    '        []gobot.Device{servo},\n' +
+    '        work,\n' +
+    '    )\n' +
+    '    gbot.AddRobot(robot)\n' +
+    '    gbot.Start()\n' +
+    '}';
+
+  });
