56a4778ade08508c985a12f69fa8a543dcc52852
Li Yiting-YITING <yiting@motorola.com>
2015-07-28 20:58:21 -0700

    change api url

--- app/scripts/services/page_counter.js
+++ app/scripts/services/page_counter.js
@@ -3,7 +3,7 @@
 angular.module('yiting007githubioApp').service('pageCounter', [
   '$http',
   function ($http) {
-    var counterRequestURL = 'https://hidden-retreat-5902.herokuapp.com/count';
+    var counterRequestURL = 'https://io-backend.herokuapp.com/count';
     this.getPageCount = function (pageId) {
       return $http({
         method: 'GET',
