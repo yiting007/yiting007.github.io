30a8ae4fba5894d7d3f120cc5ba8f4be50783f02
yiting007 <yiting.star@gmail.com>
2017-01-22 21:10:51 -0800

    dropdown test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,76 +9,80 @@
  * Main module of the application.
  */
 angular
-  .module('yiting007githubioApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'akoenig.deckgrid',
-    'dUtilApp',
-    'ui.codemirror'
-  ])
-  .config(function ($routeProvider) {
+.module('yiting007githubioApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'akoenig.deckgrid',
+        'dUtilApp',
+        'ui.codemirror'
+])
+.config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl',
+            controllerAs: 'main'
+        })
+    .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl',
         controllerAs: 'about'
-      })
-     .when('/projects/angular-date-picker', {
+    })
+    .when('/projects/angular-date-picker', {
         templateUrl: 'views/projects/dateRangePicker.html',
         controller: 'dUtilCtrl',
         controllerAs: 'date-picker'
-      })
-      .when('/projects/game-of-life', {
+    })
+    .when('/projects/game-of-life', {
         templateUrl: 'views/projects/gameOfLife.html',
         controller: 'lifeCtrl',
         controllerAs: 'game-of-life'
-      })
-      .when('/projects/arduino', {
+    })
+    .when('/projects/arduino', {
         templateUrl: 'views/projects/arduino.html',
         controller: 'arduinoCtrl',
         controllerAs: 'arduino'
-      })
-      .when('/projects/digit', {
+    })
+    .when('/projects/digit', {
         templateUrl: 'views/projects/digit.html',
         controller: 'digitCtrl',
         controllerAs: 'digit'
-      })
-      .when('/projects/sudoku', {
+    })
+    .when('/projects/sudoku', {
         templateUrl: 'views/projects/sudoku.html',
         controller: 'sudokuCtrl',
         controllerAs: 'sudoku'
-      })
-      .when('/projects/infiniteScroll', {
+    })
+    .when('/projects/infiniteScroll', {
         templateUrl: 'views/projects/infiniteScroll.html'
-      })
-     .when('/happybirthday', {
+    })
+    .when('/happybirthday', {
         templateUrl: 'views/fun/philBirthday.html',
         controller: 'philBirthdayCtrl',
         controllerAs: 'philBirthday'
-      })
-      .when('/ideas', {
-          templateUrl: 'views/fun/ideas.html',
-          controller: 'ideasCtrl',
-          controllerAs: 'ideas'
-      })
-      .when('/todo', {
-          templateUrl: 'views/fun/todo.html',
-          controller: 'todoCtrl',
-          controllerAs: 'todo'
-      })
-      .when('/notes/io-setup', {
-          templateUrl: 'views/notes/io-setup.html'
-      })
-      .otherwise({
+    })
+    .when('/ideas', {
+        templateUrl: 'views/fun/ideas.html',
+        controller: 'ideasCtrl',
+        controllerAs: 'ideas'
+    })
+    .when('/todo', {
+        templateUrl: 'views/fun/todo.html',
+        controller: 'todoCtrl',
+        controllerAs: 'todo'
+    })
+    .when('/notes/io-setup', {
+        templateUrl: 'views/notes/io-setup.html'
+    })
+    .when('/tests/dropdown', {
+        controller: 'dropdownCtrl',
+        templateUrl: 'views/fun/dropdown.html'
+    })
+    .otherwise({
         redirectTo: '/'
-      });
-  });
+    });
+});
