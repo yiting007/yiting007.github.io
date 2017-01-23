a25ba052f659ed49cc1565eb81a6aa4abea157fb
yiting007 <yiting.star@gmail.com>
2017-01-22 21:14:13 -0800

    fix warnings

--- app/scripts/directives/dropdown.js
+++ app/scripts/directives/dropdown.js
@@ -18,21 +18,21 @@ app.directive('dropdown', function($document) {
             '</div>' +
             '<span ng-show="isShowMenu" ng-click="isShowMenu = false" ng-class="classmenu" ng-style="menuStyle" ng-transclude></span>' +
             '</span>',
-        link: function(scope, elm, attrs) {
+        link: function(scope, elm) {
 
             scope.menuStyle = { 'position': 'absolute' };
 
             elm.bind('mousedown', function() {
                 // mousedown event is called earlier than click event
-                scope.menuStyle['left'] =  elm.prop('offsetLeft') + 'px';
-                scope.menuStyle['top'] =  (elm.prop('offsetTop') + elm.prop('offsetHeight')) + 'px';
+                scope.menuStyle.left =  elm.prop('offsetLeft') + 'px';
+                scope.menuStyle.top =  (elm.prop('offsetTop') + elm.prop('offsetHeight')) + 'px';
             });
 
             elm.bind('click', function(event) {
                 event.stopPropagation();
             });
 
-            $document.bind('click', function(e) {
+            $document.bind('click', function() {
                 scope.isShowMenu = false;
                 scope.$apply();
             });
